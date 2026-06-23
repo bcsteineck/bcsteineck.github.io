2fae5e95eb26582e4c4ff36959a8d0dc934ec85a
Brody <brody@Brodys-MacBook-Pro.local>
2026-06-23 15:29:54 -0500

    Initial React TypeScript Vite setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Brody Steineck</h1>
+      <p>Frontend Developer & Design Engineer</p>
+    </main>
+  )
+}
+
+export default App
